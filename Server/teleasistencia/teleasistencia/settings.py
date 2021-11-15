05b341c93953510b42abd973a0b25490f8b58e0f
fsangar <57873286+fsangar@users.noreply.github.com>
2021-11-15 12:56:51 +0100

    Añadido autentificación por defecto

--- Server/teleasistencia/teleasistencia/settings.py
+++ Server/teleasistencia/teleasistencia/settings.py
@@ -97,7 +97,10 @@ REST_FRAMEWORK = {
         'oauth2_provider.contrib.rest_framework.OAuth2Authentication',  # django-oauth-toolkit >= 1.0.0
         #'rest_framework_social_oauth2.authentication.SocialAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication'
-    )
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
 
 
