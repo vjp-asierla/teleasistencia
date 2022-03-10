a6910d93bc854197ba2222aa674acd81ed725283
vjp-ignacioDJ <idominguezj01@informatica.iesvalledeljerteplasencia.es>
2022-03-10 18:27:50 +0100

    Arreglando CORS y añadiendo requeriemiento al fichero

--- Server/teleasistencia/teleasistencia/settings.py
+++ Server/teleasistencia/teleasistencia/settings.py
@@ -108,9 +108,9 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication'
     ),
 
-    #'DEFAULT_PERMISSION_CLASSES': [
-     #   'rest_framework.permissions.IsAuthenticated',
-    #]
+    # 'DEFAULT_PERMISSION_CLASSES': [
+     #    'rest_framework.permissions.IsAuthenticated',
+    # ]
 }
 
 
