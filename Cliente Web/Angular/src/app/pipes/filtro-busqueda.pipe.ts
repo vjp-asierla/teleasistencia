29f25117cf3019ccb6bdc869342fc2d9783864c8
CFloresb09 <83606063+CFloresb09@users.noreply.github.com>
2022-04-25 20:31:48 +0200

    Correcciones de conflicto Pull Request
    Correcciones necesarias antes del Pull Request

--- Cliente Web/Angular/src/app/pipes/filtro-busqueda.pipe.ts
+++ Cliente Web/Angular/src/app/pipes/filtro-busqueda.pipe.ts
@@ -5,6 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FiltroBusquedaPipe implements PipeTransform {
 
+  /*Simplemente, vamos comprobando si el valor introducido esta presente en el contenido,
+  pasandolo a minusculas para que no sea case sensitive */
   transform(valorIntroducido: any[], filtro?: any): any {
     return valorIntroducido.filter(function (contenido){
       return JSON.stringify(contenido).toLowerCase().includes(filtro.toLowerCase());
