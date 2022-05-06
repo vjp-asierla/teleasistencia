c02d23c1173e9f9c51cfe597be965b688eebfff8
vjp-asierla <72455229+vjp-asierla@users.noreply.github.com>
2022-05-06 17:18:53 +0200

    Cambios Mensajes Alerts

--- Cliente Web/Angular/src/environments/environment.prod.ts
+++ Cliente Web/Angular/src/environments/environment.prod.ts
@@ -2,20 +2,20 @@ export const environment = {
   production: true,
 
 
-  //Tiempo que tarda en irse el mensaje de exito
+//Tiempo que tarda en irse el mensaje de exito
   timerToast : 4000,
   //Frase del Toast al Eliminar Con Exito
-  fraseEliminar: 'Elemento Eliminado Correctamente',
+  fraseEliminar: 'Se Ha Eliminado Correctamente',
   //Frase del Toast al Modificar Con Exito
-  fraseModificar: 'Elemento Modificado Correctamente',
+  fraseModificar: 'Se Ha Modificado Correctamente',
   //Frase del Toast al Crear Con Exito
-  fraseCrear: 'Elemento Creado Correctamente',
+  fraseCrear: 'Se Ha Eliminado Correctamente',
   //Frase de error al Eliminar del Toast
-  fraseErrorEliminar: 'Error al Eliminar el Elemento',
+  fraseErrorEliminar: 'Se Ha Producido Un Error Inesperado',
   //Frase de error al Modificar del Toast
-  fraseErrorModificar: 'Error al Modificar el Elemento',
+  fraseErrorModificar: 'Se Ha Producido Un Error Inesperado',
   //Frase de error al Crear del Toast
-  fraseErrorCrear: 'Error al Crear el Elemento',
+  fraseErrorCrear: 'Se Ha Producido Un Error Inesperado',
 
 
   //Modal
