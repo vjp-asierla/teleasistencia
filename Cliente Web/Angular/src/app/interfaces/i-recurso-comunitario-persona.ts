aec68b917d0f739cb5376bf2deab94fb5bb7bd47
vjp-ignacioDJ <idominguezj01@informatica.iesvalledeljerteplasencia.es>
2022-05-20 18:33:05 +0200

    Commit final retoques frederic

--- Cliente Web/Angular/src/app/interfaces/i-recurso-comunitario-persona.ts
+++ Cliente Web/Angular/src/app/interfaces/i-recurso-comunitario-persona.ts
@@ -6,13 +6,4 @@ export interface IRecursoComunitarioPersonal {
   Localidad:            string;
   Direccion:            string;
   Provincia:            string;
-  "Centro de salud":    string;
-  "Consultorio local":  string;
-  Hospital:             string;
-  "Unidad Móvil":       string;
-  Bomberos:             string;
-  "Cruz Roja":          string;
-  "Guardia Civil":      string;
-  Policía:              string;
-  "Servicios Sociales": string;
 }
